1f219272f9f52e46b685c5df2cfceae29b5cc338
agentronin316 <agentronin316@gmail.com>
2015-10-03 11:45:16 -0500

    Started Game Save Functionality
    Created placeholder scripts for Engine, BoardHex, BoardCorner, and
    BoardEdge and started framework for game saving

---
+++ MarshallTempDevProject/MarshallTempDevProject/Assets/SaveGame.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// @author Marshall Mason
+/// </summary>
+public class SaveGame : MonoBehaviour {
+
+
+    Engine engine;
+
+    void Start()
+    {
+        engine = this.gameObject.GetComponent<Engine>();
+    }
+
+
+    public void Save()
+    {
+        //loop through hexes
+        foreach (BoardHex hex in engine.hexes)
+        {
+            //export type and value
+        }
+
+        //loop through corners
+        foreach(BoardCorner corner in engine.corners)
+        {
+            //export owner
+        }
+
+        //Loop through edges
+        foreach(BoardEdge edge in engine.edges)
+        {
+            //export owner
+        }
+    }
+
+}
