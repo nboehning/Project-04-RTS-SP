c39c34ef846d9c46df8b29aca4255c8759c1f64d
nboehning <nboehning@gmail.com>
2015-10-05 16:07:28 -0500

    Created Folders and scripts
    Created folders for my work as well as the scripts I'll need

---
+++ Settlers of Not Catan/Assets/Editor/Nathans Work/ScriptMapCreationWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ScriptMapCreationWindow : EditorWindow {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
