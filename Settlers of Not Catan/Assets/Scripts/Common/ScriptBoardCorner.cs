db4849322c256ded64d5287ece28df3d1c6f11e1
agentronin316 <agentronin316@gmail.com>
2015-10-08 18:52:18 -0500

    Data Structures and Board Functionality updated
    WHARRGARBL!!!

--- Settlers of Not Catan/Assets/Scripts/Common/ScriptBoardCorner.cs
+++ Settlers of Not Catan/Assets/Scripts/Common/ScriptBoardCorner.cs
@@ -1,8 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ScriptBoardCorner : MonoBehaviour {
 
+    public ScriptEngine engine;
     public ScriptPlayer owner = null;
+    public bool roadUp;
+    public List<ScriptBoardHex> adjacentHexes = new List<ScriptBoardHex>(0);
+    public List<ScriptBoardEdge> adjacentRoads = new List<ScriptBoardEdge>(0);
 
+
+    public bool CheckValidBuild()
+    {
+        foreach(ScriptBoardEdge road in adjacentRoads)
+        {
+            if (road.owner == engine.players[0])
+            {
+                owner = engine.players[0];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void GainResources(int checkValue)
+    {
+        foreach (ScriptBoardHex hex in adjacentHexes)
+        {
+            if (hex.hexDieValue == checkValue)
+            {
+                switch(hex.resource)
+                {
+                    case HexType.BRICK:
+                        owner.AddBricks(1);
+                        break;
+                    case HexType.GRAIN:
+                        owner.AddWheat(1);
+                        break;
+                    case HexType.WOOD:
+                        owner.AddLumber(1);
+                        break;
+                    case HexType.WOOL:
+                        owner.AddWool(1);
+                        break;
+                }
+            }
+        }
+    }
 }
