e5a0b5e870892e07df56038a2053098d58d37ab5
agentronin316 <agentronin316@gmail.com>
2015-10-10 12:52:13 -0500

    Finished Save and Load
    also finished data structures in the board and created a second board
    prefab with all the pieces interconnected

--- Settlers of Not Catan/Assets/Scripts/Common/ScriptBoardHex.cs
+++ Settlers of Not Catan/Assets/Scripts/Common/ScriptBoardHex.cs
@@ -24,23 +24,23 @@ public class ScriptBoardHex : MonoBehaviour
     //-56.03992 /
     //56.03992  \
     //0   	  |
-    public const float MAGIC_EDGE_DISTANCE_CONVERT = 0.86602540378f; //Mathf.Sqrt(.75f) 
+    public const float MAGIC_EDGE_DISTANCE_CONVERT = 0.86602540378f; //Mathf.Sqrt(.75f)
 
     public GameObject cornerPrefab;
     public GameObject edgePrefab;
-    public ScriptEngine engine;
     public float hexSideLength;
     public int hexDieValue;
     public HexType resource;
 
-    List<ScriptBoardCorner> cornerScripts = new List<ScriptBoardCorner>(0);
+    List<ScriptBoardEdge> edgeScripts = new List<ScriptBoardEdge>(0);
 
     void Start()
     {
-        engine = GameObject.Find("Game Engine").GetComponent<ScriptEngine>();
+        CheckAndGenerateCorners();
+        CheckAndGenerateEdges();
+        ScriptSaveLoad saveScript = GameObject.Find("GameEngine").GetComponent<ScriptSaveLoad>();
+        saveScript.hexes.Add(this);
     }
-
-
 
     public void CheckAndGenerateEdges()
     {
@@ -54,14 +54,14 @@ public class ScriptBoardHex : MonoBehaviour
                                     (center.y + edgeDistance * Mathf.Sin(angle)), (center.z - .5f));
             //@endRef Nathan
 
-
-            Collider[] hitColliders = Physics.OverlapSphere(cornerPos, .125f);
+            Collider[] hitColliders = Physics.OverlapSphere(cornerPos, .15f);
             bool edgeFound = false;
             foreach (Collider other in hitColliders)
             {
                 if (other.tag == "Road")
                 {
                     edgeFound = true;
+                    
                 }
             }
             if (!edgeFound)
@@ -83,123 +83,25 @@ public class ScriptBoardHex : MonoBehaviour
             //@endRef Nathan
 
 
-            Collider[] hitColliders = Physics.OverlapSphere(cornerPos, .125f);
+            Collider[] hitColliders = Physics.OverlapSphere(cornerPos, .15f);
             bool settlementFound = false;
             foreach (Collider other in hitColliders)
             {
                 if (other.tag == "Settlement")
                 {
                     settlementFound = true;
-                    cornerScripts.Add(other.gameObject.GetComponent<ScriptBoardCorner>());
+                    if (!other.GetComponent<ScriptBoardCorner>().adjacentHexes.Contains(this))
+                    {
+                        other.gameObject.GetComponent<ScriptBoardCorner>().adjacentHexes.Add(this);
+                    }
                 }
             }
             if (!settlementFound)
             {
                 GameObject temp = (GameObject)Instantiate(cornerPrefab, cornerPos, Quaternion.identity);
-                cornerScripts.Add(temp.GetComponent<ScriptBoardCorner>());
+                temp.gameObject.GetComponent<ScriptBoardCorner>().adjacentHexes.Add(this);
             }
         }
     }
+
 }
-
-//using UnityEngine;
-//using System.Collections;
-//using System.Collections.Generic;
-
-///// <summary>
-///// @author Marshall Mason
-///// </summary>
-//public class ScriptBoardHex : MonoBehaviour {
-
-//    //Edge angle notes
-//    //Z rotation
-
-//    //-56.03992 /
-//    //56.03992  \
-//    //0   	  |
-
-
-//    public GameObject cornerPrefab;
-//    public GameObject edgePrefab;
-//    public ScriptEngine engine;
-//    public float hexSideLength;
-//    public int hexDieValue;
-//    public HexType resource;
-
-//    List<ScriptBoardCorner> cornerScripts = new List<ScriptBoardCorner>(0);    
-    
-//    void Start()
-//    {
-//        engine = GameObject.Find("Game Engine").GetComponent<ScriptEngine>();
-//    }
-
-
-//    public void GrantResources()
-//    {
-//        foreach (ScriptBoardCorner corner in cornerScripts)
-//        {
-//            //if (corner.owner.PlayerName == engine.players[0].PlayerName)
-//            //{
-//            //    //tell the settlement to add 1 of blah resources to owning player.
-//            //}
-//        }
-//    }
-
-//    public void CheckAndGenerateEdges()
-//    {
-//        Vector3 center = transform.position;
-//        for (int i = 0; i < 6; i++)
-//        {
-//            //@ref Nathan
-//            float angle = Mathf.PI * (60f * i + 30) / 180;
-//            Vector3 cornerPos = new Vector3((center.x + hexSideLength * Mathf.Cos(angle)),
-//                                    (center.y + hexSideLength * Mathf.Sin(angle)), (center.z - .5f));
-//            //@endRef Nathan
-
-
-//            Collider[] hitColliders = Physics.OverlapSphere(cornerPos, .125f);
-//            bool edgeFound = false;
-//            foreach (Collider other in hitColliders)
-//            {
-//                if (other.tag == "Road")
-//                {
-//                    edgeFound = true;
-//                }
-//            }
-//            if (!edgeFound)
-//            {
-//                GameObject temp = (GameObject)Instantiate(edgePrefab, cornerPos, Quaternion.identity);
-//            }
-//        }
-//    }
-    
-//    public void CheckAndGenerateCorners()
-//    {
-//        Vector3 center = transform.position;
-//        for (int i = 0; i < 6; i++)
-//        {
-//            //@ref Nathan
-//            float angle = Mathf.PI * (60f * i) / 180;
-//            Vector3 cornerPos = new Vector3((center.x + hexSideLength * Mathf.Cos(angle)),
-//                                    (center.y + hexSideLength * Mathf.Sin(angle)), (center.z - 1));
-//            //@endRef Nathan
-
-
-//            Collider[] hitColliders = Physics.OverlapSphere(cornerPos, .125f);
-//            bool settlementFound = false;
-//            foreach (Collider other in hitColliders)
-//            {
-//                if (other.tag == "Settlement")
-//                {
-//                    settlementFound = true;
-//                    cornerScripts.Add(other.gameObject.GetComponent<ScriptBoardCorner>());
-//                }
-//            }
-//            if(!settlementFound)
-//            {
-//                GameObject temp = (GameObject)Instantiate(cornerPrefab, cornerPos, Quaternion.identity);
-//                cornerScripts.Add(temp.GetComponent<ScriptBoardCorner>());
-//            }
-//        }
-//    }
-//}
