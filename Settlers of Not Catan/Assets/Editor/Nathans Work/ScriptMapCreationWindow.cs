12050579e24744d37afa8771603194fcd40172f0
nboehning <nboehning@gmail.com>
2015-10-11 00:09:32 -0500

    Updated the editor window
    Created a jagged array (non rectangular 2d array) to hold all of the hex
    data. Added code to draw the hex shapes on the window.

--- Settlers of Not Catan/Assets/Editor/Nathans Work/ScriptMapCreationWindow.cs
+++ Settlers of Not Catan/Assets/Editor/Nathans Work/ScriptMapCreationWindow.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEditor;
 
 public class ScriptMapCreationWindow : EditorWindow
 {
 
-    private Vector2 mapDataScrollPos = Vector2.zero;
+    // Hex map variables
+    private ScriptHex[][] hexMap = new ScriptHex[15][];
+    private int selectedRow = 7;
+    private int selectedColumn = 7;
+
+
+    // Drawing of map variables
     private int numRows = 15;
     private int hexEdgeLength = 30;
     private Rect labelPosition;
@@ -12,17 +19,18 @@ public class ScriptMapCreationWindow : EditorWindow
     private float yOffset = 200f;
     private string labelText;
     private Vector2 hexCenter;
-    private Color oldColor;
-    private ScriptHex selectedHex = new ScriptHex(Vector2.zero, 2);
-    private int curHex = 0;
-    private ScriptHex[] hexMap = new ScriptHex[169];
+    private const float MAGIC_EDGE_DISTANCE_CONVERT = 0.86602540378f;   //Mathf.Sqrt(0.75f) & -Sin(Mathf.PI * 4 / 3) (apparently) Marshallllllll
+
+    // Hex data variables
     string[] intPopupString = { "1", "2", "3", "4", "5", "6" };
     int[] intPopup = { 1, 2, 3, 4, 5, 6 };
+    private string hexName = string.Format("Hex ({0}, {1})", 7, 7);
+    
+    // Stuff for heuristics
     private int numWool;
     private int numWood;
     private int numBrick;
     private int numWheat;
-    private const float MAGIC_EDGE_DISTANCE_CONVERT = 0.86602540378f;   //Mathf.Sqrt(0.75f) & -Sin(Mathf.PI * 4 / 3) (apparently) Marshallllllll
 
     [MenuItem("Tools/Create New Map")]
     private static void Init()
@@ -32,92 +40,51 @@ public class ScriptMapCreationWindow : EditorWindow
         window.position = new Rect(100, 50, 1000, 750);
         window.maxSize = new Vector2(1000, 750);
         window.minSize = window.maxSize;
+        window.InitializeVariables();
         window.Show();
     }
 
     void OnGUI()
     {
-        xOffset = 400f;
-        yOffset = 45f;
+        xOffset = 375f;
+        yOffset = 50f;
         EditorGUILayout.BeginHorizontal();
 
-        // @author: MARSHALL AND HIS MATH GODLINESS, plus nathan
-        #region Map
-        EditorGUILayout.BeginVertical("box", GUILayout.Width(position.width * 0.99f), GUILayout.Height(position.height - 40f));
+        EditorGUILayout.BeginVertical("box", GUILayout.Width(position.width - 5), GUILayout.Height(position.height - 40f));
 
-        EditorGUILayout.LabelField("                                        " +
-                                   "                                                                                          " +
-                                   "      Map", EditorStyles.boldLabel);
-        #region vertical spacing
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-        #endregion
         EditorGUILayout.BeginVertical();
         #region Map Data
 
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.BeginVertical();
-        EditorGUILayout.LabelField("Hex Data", EditorStyles.boldLabel, GUILayout.Width(65f));
+        Rect hexDataRect = new Rect(100f, 100f, 65f, 15f);
+        EditorGUI.LabelField(hexDataRect, "Hex Data", EditorStyles.boldLabel);
 
+        Rect hexNameRect = new Rect(100f, 117f, 65f, 15f);
+        EditorGUI.LabelField(hexNameRect, hexName);
 
-        EditorGUILayout.LabelField("Roll Value: ", GUILayout.Width(60f));
-        selectedHex.hexNum = EditorGUILayout.IntPopup("", selectedHex.hexNum, intPopupString, intPopup, GUILayout.Width(25f));
+        Rect rollValueLabelRect = new Rect(100f, 134f, 70f, 15f);
+        EditorGUI.LabelField(rollValueLabelRect, "Roll Value: ");
 
-        EditorGUILayout.LabelField("Hex Type: ", GUILayout.Width(60f));
-        selectedHex.hexType = (HexType)EditorGUILayout.EnumPopup(selectedHex.hexType, GUILayout.Width(100f));
-        EditorGUILayout.EndVertical();
-        EditorGUILayout.EndHorizontal();
+        Rect rollDropDownRect = new Rect(174f, 134f, 25f, 17f);
+        hexMap[selectedRow][selectedColumn].hexNum = EditorGUI.IntPopup(rollDropDownRect, hexMap[selectedRow][selectedColumn].hexNum, intPopupString, intPopup);
+
+        Rect hexTypeLabelRect = new Rect(100f, 151f, 60f, 15f);
+        EditorGUI.LabelField(hexTypeLabelRect, "Hex Type: ");
+
+        Rect hexTypeRect = new Rect(174f, 134f, 25f, 17f);
+        hexMap[selectedRow][selectedColumn].hexType = (HexType)EditorGUI.EnumPopup(hexTypeRect, hexMap[selectedRow][selectedColumn].hexType);
+
         #endregion
 
         EditorGUILayout.EndVertical();
 
-        // yay marshall
+
+        GUIStyle style = EditorStyles.label;
+
+        style.alignment = TextAnchor.MiddleCenter;
+
+
+        // @author: MARSHALL AND HIS MATH GODLINESS, plus nathan
+        #region Map
         for (int i = 0; i < numRows; i++)
         {
             switch (i)
@@ -135,21 +102,43 @@ public class ScriptMapCreationWindow : EditorWindow
                         labelPosition = new Rect(xOffset, yOffset, 40f, 15f);
                         hexCenter.x = xOffset + 20f;
                         hexCenter.y = yOffset + 8f;
-                        EditorGUI.LabelField(labelPosition, labelText);
-                        ScriptHex hex = new ScriptHex(hexCenter, hexEdgeLength);
-
-                        for (int k = 0; k < 5; k++)
+                        
+                        if (GUI.Button(labelPosition, labelText, style))
                         {
-                            // Draw line from hex.hexCorners[k] to hex.hexCorners[k + 1]
+
+                            selectedRow = i;
+                            selectedColumn = j;
+                            hexName = string.Format("Hex ({0}, {1})", i, j);
+
                         }
 
-                        // Draw last line from hex.hexCorner[5] to hex.hexCorners[0]
+                        ScriptHex hex = new ScriptHex(hexCenter, hexEdgeLength);
+
+                        #region draw the hex
+                        // Vertical lines
+                        Drawing.DrawLine(hex.hexCorners[2], hex.hexCorners[3], Color.black, 1f, true);
+                        Drawing.DrawLine(hex.hexCorners[0], hex.hexCorners[5], Color.black, 1f, true);
+
+                        // Bottom of hex
+                        hex.hexCorners[0].x += 13f;
+                        hex.hexCorners[1].x += 13f;
+                        hex.hexCorners[2].x += 13f;
+                        Drawing.DrawLine(hex.hexCorners[0], hex.hexCorners[1], Color.black, 1f, true);
+                        Drawing.DrawLine(hex.hexCorners[1], hex.hexCorners[2], Color.black, 1f, true);
+
+                        // Top of hex
+                        hex.hexCorners[3].x -= 13f;
+                        hex.hexCorners[4].x -= 13f;
+                        hex.hexCorners[5].x -= 13f;
+                        Drawing.DrawLine(hex.hexCorners[3], hex.hexCorners[4], Color.black, 1f, true);
+                        Drawing.DrawLine(hex.hexCorners[4], hex.hexCorners[5], Color.black, 1f, true);
+                        #endregion
 
                         xOffset += hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f;
                     }
                     xOffset -= (hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f) * (i + 8);
                     xOffset += (hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f) * (Mathf.Cos(Mathf.PI * (4f / 3f)));
-                    yOffset += (hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f) * MAGIC_EDGE_DISTANCE_CONVERT;
+                    yOffset += (hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f) * MAGIC_EDGE_DISTANCE_CONVERT - 2;
                     break;
                 case 7:
                 case 8:
@@ -165,21 +154,44 @@ public class ScriptMapCreationWindow : EditorWindow
                         labelPosition = new Rect(xOffset, yOffset, 40f, 15f);
                         hexCenter.x = xOffset + 20f;
                         hexCenter.y = yOffset + 8f;
-                        EditorGUI.LabelField(labelPosition, labelText);
-                        ScriptHex hex = new ScriptHex(hexCenter, hexEdgeLength);
 
-                        for (int k = 0; k < 5; k++)
+                        if (GUI.Button(labelPosition, labelText, style))
                         {
-                            // Draw line from hex.hexCorners[k] to hex.hexCorners[k + 1]
+
+                            selectedRow = i;
+                            selectedColumn = j;
+                            hexName = string.Format("Hex ({0}, {1})", i, j);
+
                         }
 
-                        // Draw last line from hex.hexCorner[5] to hex.hexCorners[0]
+                        ScriptHex hex = new ScriptHex(hexCenter, hexEdgeLength);
+
+                        #region draw the hex
+                        // Vertical lines
+                        Drawing.DrawLine(hex.hexCorners[2], hex.hexCorners[3], Color.black, 1f, true);
+                        Drawing.DrawLine(hex.hexCorners[0], hex.hexCorners[5], Color.black, 1f, true);
+
+                        // Bottom of hex
+                        hex.hexCorners[0].x += 13f;
+                        hex.hexCorners[1].x += 13f;
+                        hex.hexCorners[2].x += 13f;
+                        Drawing.DrawLine(hex.hexCorners[0], hex.hexCorners[1], Color.black, 1f, true);
+                        Drawing.DrawLine(hex.hexCorners[1], hex.hexCorners[2], Color.black, 1f, true);
+
+                        // Top of hex
+                        hex.hexCorners[3].x -= 13f;
+                        hex.hexCorners[4].x -= 13f;
+                        hex.hexCorners[5].x -= 13f;
+                        Drawing.DrawLine(hex.hexCorners[3], hex.hexCorners[4], Color.black, 1f, true);
+                        Drawing.DrawLine(hex.hexCorners[4], hex.hexCorners[5], Color.black, 1f, true);
+
+                        #endregion
 
                         xOffset += hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f;
                     }
                     xOffset -= (hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f) * (22 - i);
                     xOffset -= (hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f) * (Mathf.Cos(Mathf.PI * (4f / 3f)));
-                    yOffset -= (hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f) * (Mathf.Sin(Mathf.PI * (4f / 3f)));
+                    yOffset -= (hexEdgeLength * MAGIC_EDGE_DISTANCE_CONVERT * 2f) * (Mathf.Sin(Mathf.PI * (4f / 3f))) + 2;
                     break;
             }
         }
@@ -189,6 +201,38 @@ public class ScriptMapCreationWindow : EditorWindow
         EditorGUILayout.EndVertical();
 
         EditorGUILayout.EndHorizontal();
+
+    }
+
+    void InitializeVariables()
+    {
+        hexMap[0] = new ScriptHex[8];
+        hexMap[1] = new ScriptHex[9];
+        hexMap[2] = new ScriptHex[10];
+        hexMap[3] = new ScriptHex[11];
+        hexMap[4] = new ScriptHex[12];
+        hexMap[5] = new ScriptHex[13];
+        hexMap[6] = new ScriptHex[14];
+        hexMap[7] = new ScriptHex[15];
+        hexMap[8] = new ScriptHex[14];
+        hexMap[9] = new ScriptHex[13];
+        hexMap[10] = new ScriptHex[12];
+        hexMap[11] = new ScriptHex[11];
+        hexMap[12] = new ScriptHex[10];
+        hexMap[13] = new ScriptHex[9];
+        hexMap[14] = new ScriptHex[8];
+       
+        for (int i = 0; i < hexMap.Length; i++)
+        {
+            
+            for (int j = 0; j < hexMap[i].Length; j++)
+            {
+                hexMap[i][j] = new ScriptHex();
+            }
+        }
+
+        hexMap[7][7].isActive = true;
+        hexMap[7][7].hexType = HexType.WOOD;
 
     }
 }
